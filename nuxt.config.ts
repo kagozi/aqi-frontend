e87deb43dcd9aa5e43bea6e67ee4f83fc7223b1c
ALEX KAGOZI <alexkagozi@ALEXs-MacBook-Pro.local>
2025-06-23 21:21:21 -0700

    ++ Initial Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  modules:['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  css: ['~/assets/css/main.css'],
+  devtools: { enabled: true },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
